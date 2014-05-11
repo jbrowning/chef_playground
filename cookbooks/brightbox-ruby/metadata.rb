88b320064537c279acb61cdccd03ef4be9d1390a
Jeff Browning <jeff@jkbrowning.com>
2014-05-10 22:00:30 -0400

    Initial commit

---
+++ cookbooks/brightbox-ruby/metadata.rb
@@ -0,0 +1,52 @@
+require 'chef'
+
+name              'brightbox-ruby'
+maintainer        'Mojo Lingo LLC'
+maintainer_email  'ops@mojolingo.com'
+license           'Apache 2.0'
+description       'Handles managing Rubies from brightbox'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '1.0.0'
+
+recipe "brightbox-ruby::default", "Adds the brightbox repository, installs the Ruby package, sets it as the default then installs a sane server gemrc and adds bundler, rake and rubygems-bundler gems to bootstrap your environment."
+
+grouping 'brightbox-ruby',
+ title: "Ruby install options",
+ description: "Set options relevant to installing Ruby"
+
+attribute 'brightbox-ruby/default_action',
+ display_name: "Default action for Ruby install",
+ description: "Default action for Ruby install",
+ choice: (Chef::Version.new(Chef::VERSION) >= Chef::Version.new("11.12")) ? [:upgrade, :install] : [':upgrade', ':install'],
+ type: "symbol",
+ required: "optional",
+ recipes: ['brightbox-ruby'],
+ default: (Chef::Version.new(Chef::VERSION) >= Chef::Version.new("11.12")) ? :upgrade : ':upgrade'
+
+attribute 'brightbox-ruby/version',
+ display_name: "The version of Ruby to install",
+ description: "The version of Ruby to install",
+ type: "string",
+ required: "optional",
+ recipes: ['brightbox-ruby'],
+ default: '2.1'
+
+attribute 'brightbox-ruby/install_dev_package',
+ display_name: "Install the dev package, which provides headers for gem native extensions",
+ description: "Install the dev package, which provides headers for gem native extensions",
+ choice: ['true', 'false'],
+ required: "optional",
+ recipes: ['brightbox-ruby'],
+ default: 'true'
+
+attribute 'brightbox-ruby/gems',
+ display_name: "Gems to be installed by default",
+ description: "Gems to be installed by default",
+ type: "array",
+ required: "optional",
+ recipes: ['brightbox-ruby'],
+ default: ["bundler", "rake", "rubygems-bundler"]
+
+supports 'ubuntu'
+
+depends 'apt'
